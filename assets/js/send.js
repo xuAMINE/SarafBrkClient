bd43caa194449a21244fc6d051b3ce0a6926a8cf
Mohamed Amine Zirek <uhammudz@gmail.com>
2025-05-12 16:23:54 -0700

    added rate display to the send pages

--- assets/js/send.js
+++ assets/js/send.js
@@ -167,6 +167,46 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+// Manage rate display
+document.addEventListener("DOMContentLoaded", function() {
+  const rateElement = document.getElementById('rate-display');
+  const duration = 1500; // Duration of the counting in milliseconds
+  const dropdownMenuAccount = document.getElementById('accountName'); 
+
+  function fetchRate() {
+    apiClient.get('/api/v1/rate')
+      .then(response => {
+        const targetNumber = response.data; // Assuming the API response is a number
+        animateCountUp(targetNumber);
+      })
+      .catch(error => {
+        console.error('Error fetching rate:', error);
+      });
+  }
+
+  // Function to animate the count up
+  function animateCountUp(targetNumber) {
+    let start = 0;
+    const increment = targetNumber / (duration / 10);
+
+    function countUp() {
+      start += increment;
+      if (start > targetNumber) {
+        start = targetNumber;
+        rateElement.textContent = Math.floor(start);
+      } else {
+        rateElement.textContent = Math.floor(start);
+        requestAnimationFrame(countUp);
+      }
+    }
+
+    countUp();
+  }
+
+  fetchRate();
+});
+
+
 
 // compy Phone Number
 document.getElementById('copyNumber').addEventListener('click', () => {
